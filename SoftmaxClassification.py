43f8733481f9d23286ea6474aa4b862e1fe25c7a
DailyVy <traumen25@gmail.com>
2022-07-26 11:08:22 +0900

    softmax Classification ver2. check argmax

--- SoftmaxClassification.py
+++ SoftmaxClassification.py
@@ -45,18 +45,37 @@ history = model.fit(x_data, y_data, epochs=2000)
 # Testing & One-hot encoding
 print("=====================================")
 a = model.predict(np.array([[1, 11, 7, 9]]))
-print(a, K.eval(tf.argmax(a, axis = 1)))
+print(a, K.eval(tf.argmax(a, axis = 1))) # 1
+print(a, tf.argmax(a, axis = 1)) # tf.Tensor([1], shape=(1,), dtype=int64)
 
 print("=====================================")
 b = model.predict(np.array([[1, 3, 4, 3]]))
-print(b, K.eval(tf.argmax(b, axis = 1)))
+print(b, K.eval(tf.argmax(b, axis = 1))) # 0
 
 print("=====================================")
 c = model.predict(np.array([[1, 1, 0, 1]]))
 c_onehot = np.argmax(c, axis=-1) # 얜 왜 -1인가
-print(c, c_onehot)
+print(c, c_onehot) # 2
 
 print("=====================================")
-all = model.predict(np.array([[1, 11, 7, 9]]))
+all = model.predict(np.array([[1, 11, 7, 9], [1, 3, 4, 3], [1, 1, 0, 1]]))
 all_onehot = np.argmax(all, axis=-1) # 얜 왜 -1인가
-print(all, all_onehot)
+print(all, all_onehot) # [1 0 2] 앞의 결과와 같다.
+"""
+np.argmax
+
+1. axis = None, 즉 axis를 지정하지 않은 경우 
+모든 원소를 순서대로 1차원 array에 편 상태를 가정하고 argmax를 적용한 결과를 반환
+2. axis = 1, 각 가로축 원소들끼리 비교 => 한 행을 기준으로 비교
+3. aixs = 0, 세로축 원소들끼리 비교 => 한 열을 기준으로 비교
+
+# 2차원 array 예시
+b = np.array([[4, 3, 2], [8, 5, 9], [7, 6, 1]])
+np.argmax(b) # 5
+np.argmax(b, axis = 1) # array([0, 2, 0])
+np.argmax(b, axis = 0) # array([1, 2, 1])
+
+4. axis = -1 뒤쪽 축부터 인덱싱 => 1번축
+5. axis = -2 => 0번축
+  
+"""
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+b = np.array([[4, 3, 5], [8, 5, 3], [7, 9, 1]])
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 뒤쪽 축부터 인덱싱
+print(b.argmax()) # 7
+print(b.argmax(axis = 1)) # array([2, 0, 1])
+print(b.argmax(axis = 0)) # array([1, 2, 0])
+print(b.argmax(axis = -1)) # array([2, 2, 0])
+print(b.argmax(axis = -2)) # array([1, 2, 0])
